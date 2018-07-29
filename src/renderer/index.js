11cb8aa91ab586b5d2731fbc0d017ac06b5a06b1
jorgeer <jfemail3@gmail.com>
2018-07-29 11:58:38 +0200

    format code

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -1,29 +1,29 @@
-import Vue from 'vue'
-import VueElectron from 'vue-electron'
-import axios from 'axios'
-import App from './App'
-import router from './router'
-import store from './store'
-import PouchDB from "pouchdb"
-import Vuetify from 'vuetify'
+import Vue from "vue";
+import VueElectron from "vue-electron";
+import axios from "axios";
+import App from "./App";
+import router from "./router";
+import store from "./store";
+import PouchDB from "pouchdb";
+import Vuetify from "vuetify";
 
-import 'vuetify/dist/vuetify.min.css'
-import 'material-design-icons-iconfont/dist/material-design-icons.css'
+import "vuetify/dist/vuetify.min.css";
+import "material-design-icons-iconfont/dist/material-design-icons.css";
 
-const db = new PouchDB('eldb');
+const db = new PouchDB("eldb");
 
-if (!process.env.IS_WEB) Vue.use(VueElectron)
-Vue.use(Vuetify)
+if (!process.env.IS_WEB) Vue.use(VueElectron);
+Vue.use(Vuetify);
 
-Vue.http = Vue.prototype.$http = axios
-Vue.db = Vue.prototype.$db = db
+Vue.http = Vue.prototype.$http = axios;
+Vue.db = Vue.prototype.$db = db;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
 new Vue({
   components: { App },
   router,
   store,
-  template: '<App/>'
-}).$mount('#app')
+  template: "<App/>"
+}).$mount("#app");
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,34 +1,33 @@
-import Vue from 'vue'
-import Router from 'vue-router'
+import Vue from "vue";
+import Router from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
 export default new Router({
   routes: [
     {
-      path: '/',
-      component: require('@/components/layouts/LayoutDefault').default,
+      path: "/",
+      component: require("@/components/layouts/LayoutDefault").default,
       children: [
         {
-          path: 'dashboard',
-          alias: '',
-          component: require('@/components/DashboardView').default,
-          name: 'Dashboard',
-          meta: {description: 'Overview of environment'}
+          path: "dashboard",
+          alias: "",
+          component: require("@/components/DashboardView").default,
+          name: "Dashboard",
+          meta: { description: "Overview of environment" }
         },
         {
-          path: 'settings',
-          alias: '',
-          component: require('@/components/SettingsView').default,
-          name: 'Settings',
-          meta: {description: 'Settings'}
-        },
-        
+          path: "settings",
+          alias: "",
+          component: require("@/components/SettingsView").default,
+          name: "Settings",
+          meta: { description: "Settings" }
+        }
       ]
     },
     {
-      path: '*',
-      redirect: '/'
+      path: "*",
+      redirect: "/"
     }
   ]
-})
+});
